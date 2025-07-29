a56b5b9eb9fcecb98b73daee427fb0aae583c19c
AnshBalar429 <anshbalar2910@gmail.com>
2025-07-29 11:14:15 +0530

    Add video page

---
+++ frontend/app/video/[videoId]/page.tsx
@@ -0,0 +1,12 @@
+import VideoPlayer from "@/components/VideoPlayer";
+
+
+interface VideoPageProps {
+    params: Promise<{ videoId: string }>;
+}
+
+export default async function VideoPage({ params }: VideoPageProps) {
+    const { videoId } = await params;
+
+    return <VideoPlayer videoId={videoId} />;
+}
